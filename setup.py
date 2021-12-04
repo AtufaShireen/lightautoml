c0232c26160df26244e503403dc934b3de4e5bbe
AtufaShireen <atufashireen@gmail.com>
2021-12-05 00:50:51 +0530

    preprocess

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='light-automl',
-    version='1.0',
+    version='0.2',
     packages=find_packages(where="automl"),
     author='Atufa Shireen',
     package_dir={"": "automl"},
